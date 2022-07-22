d1e3accb7a2e21ccc3dff7b3d3535b5b851b8522
Jiin k <kji306301@gmail.com>
2022-07-23 00:08:34 +0900

    중간 정리퍄

--- src/main/java/com/bestresto/springboot/Application.java
+++ src/main/java/com/bestresto/springboot/Application.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication	// {1}
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);	// {2}
+
+        SpringApplication.run(Application.class, args);
+
     }
 }
